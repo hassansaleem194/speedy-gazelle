893bd107a2582568271798449c09c8686bb0caac
Hassan <hassansaleem194@gmail.com>
2022-03-12 17:10:51 +0000

    first commit

---
+++ src/main/java/com/example/speedygazelle/SpeedyGazelleApplication.java
@@ -0,0 +1,13 @@
+package com.example.speedygazelle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedyGazelleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedyGazelleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/speedygazelle/SpeedyGazelleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.speedygazelle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedyGazelleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
